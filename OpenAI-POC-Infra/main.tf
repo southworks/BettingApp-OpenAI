f6276792ac4485467dbcd6cbe1d039533f77da7b
sol-stiep <sol.stiep@southworks.com>
2024-12-10 14:05:43 +0100

    Apply renames

--- OpenAI-POC-Infra/main.tf
+++ OpenAI-POC-Infra/main.tf
@@ -10,7 +10,7 @@ module "network" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "<container_registry_name>"
+  name                = "<azure_container_registry_name>"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   # checkov:skip=CKV_AZURE_139:Public access for simplicity
